8245d0eb1884d8b00d3965b9c0a86d212244d8ad
theorder <2486711860@qq.com>
2025-04-12 21:15:58 +0800

    map

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
@@ -12,4 +12,6 @@ import './assets/fonts/iconfont.css'
 // 注意：我们自己写的全局样式需要放在组件库样式后面导入，这样，样式才会生效！因为后面的样式会覆盖前面同名的样式
 import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+// ReactDOM.render(<App />, document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,21 +1,14 @@
 import React from 'react'
+// 导入路由
 import { Route } from 'react-router-dom'
+// 导入 TabBar
+import { TabBar } from 'antd-mobile'
 
+// 导入TabBar菜单的组件
 import News from '../News'
 import Index from '../Index'
 import HouseList from '../HouseList'
 import Profile from '../Profile'
-
-// 导入 TabBar
-import { TabBar } from 'antd-mobile'
-
-/* 
-  1 根据 TabBar 组件 文档 设置不渲染内容部分（只保留菜单项，不显示内容）。
-  2 给 TabBar.Item 绑定点击事件。
-  3 在点击事件中调用 history.push() 实现路由切换。
-  4 创建 TabBar 组件菜单项对应的其他 3 个组件，并在 Home 组件中配置路由信息。
-  5 给菜单项添加 selected 属性，设置当前匹配的菜单项高亮。
-*/
 
 // 导入组件自己的样式文件
 import './index.css'
@@ -25,7 +18,7 @@ const tabItems = [
   {
     title: '首页',
     icon: 'icon-ind',
-    path: '/home/index'
+    path: '/home'
   },
   {
     title: '找房',
@@ -44,10 +37,35 @@ const tabItems = [
   }
 ]
 
+/* 
+  问题：点击首页导航菜单，导航到 找房列表 页面时，找房菜单没有高亮
+
+  原因：原来我们实现该功能的时候，只考虑了 点击 以及 第一次加载 Home 组件的情况。但是，我们没有考虑不重新加载 Home 组件时的路由切换，因为，这种情况下，我们的代码没有覆盖到
+
+  解决：
+    思路：在 路由切换 时，也执行 菜单高亮 的逻辑代码
+    1 添加 componentDidUpdate 钩子函数
+    2 在钩子函数中判断路由地址是否切换（因为路由的信息是通过 props 传递给组件的，所以，通过比较更新前后的两个props）
+    3 在路由地址切换时，让 菜单高亮
+*/
+
 export default class Home extends React.Component {
   state = {
     // 默认选中的TabBar菜单项
     selectedTab: this.props.location.pathname
+  }
+
+  // 组件接收到新的props（此处，实际上是路由信息）就会触发该钩子函数
+  componentDidUpdate(prevProps) {
+    // prevProps 上一次的props，此处也就是上一次的路由信息
+    // this.props 当前最新的props，此处也就是最新的路由信息
+    // 注意：在该钩子函数中更新状态时，一定要在 条件判断 中进行，否则会造成递归更新的问题
+    if (prevProps.location.pathname !== this.props.location.pathname) {
+      // 此时，就说明路由发生切换了
+      this.setState({
+        selectedTab: this.props.location.pathname
+      })
+    }
   }
 
   // 渲染 TabBar.Item
@@ -77,7 +95,7 @@ export default class Home extends React.Component {
       <div className="home">
         {/* 2.3 渲染子路由 */}
         <Route path="/home/news" component={News} />
-        <Route path="/home/index" component={Index} />
+        <Route exact path="/home" component={Index} />
         <Route path="/home/list" component={HouseList} />
         <Route path="/home/profile" component={Profile} />
         {/* TabBar */}
