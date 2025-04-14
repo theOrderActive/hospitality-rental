25aecb057afbbd9050bf38e3db53d86318559d15
theorder <2486711860@qq.com>
2025-04-14 22:30:58 +0800

    CITY

--- src/pages/CityList/index.js
+++ src/pages/CityList/index.js
@@ -1,14 +1,22 @@
 import React from 'react'
 
 import axios from 'axios'
-import { NavBar } from 'antd-mobile'
+import { Toast } from 'antd-mobile'
 
 // 导入 List 组件
 import { List, AutoSizer } from 'react-virtualized'
 
-import './index.scss'
 // 导入 utils 中获取当前定位城市的方法
 import { getCurrentCity } from '../../utils'
+
+// 导入 NavHeader 组件
+import NavHeader from '../../components/NavHeader'
+
+import './index.scss'
+
+// 导入 CSSModules 的样式文件
+// import styles from './index.module.css'
+// console.log(styles)
 
 // 数据格式化的方法
 // list: [{}, {}]
@@ -40,39 +48,6 @@ const formatCityData = list => {
   }
 }
 
-/* 
-  1 将获取到的 cityList 和 cityIndex  添加为组件的状态数据。
-  2 修改 List 组件的 rowCount 为 cityIndex 的长度。
-  3 将 rowRenderer 函数，添加到组件中，以便在函数中获取到状态数据 cityList 和 cityIndex。
-  4 修改 List 组件的 rowRenderer 为组件中的 rowRenderer 方法。
-  5 修改 rowRenderer 方法中渲染的每行结构和样式。
-  6 修改 List 组件的 rowHeight 为函数，动态计算每一行的高度（因为每一行高度都不相同）。
-  
-  <div key={key} style={style} className="city">
-    <div className="title">S</div>
-    <div className="name">上海</div>
-  </div>
-*/
-
-// 列表数据的数据源
-// const list = Array(100).fill('react-virtualized')
-
-// 渲染每一行数据的渲染函数
-// 函数的返回值就表示最终渲染在页面中的内容
-// function rowRenderer({
-//   key, // Unique key within array of rows
-//   index, // 索引号
-//   isScrolling, // 当前项是否正在滚动中
-//   isVisible, // 当前项在 List 中是可见的
-//   style // 注意：重点属性，一定要给每一个行数据添加该样式！作用：指定每一行的位置
-// }) {
-//   return (
-//     <div key={key} style={style}>
-//       1232 -{list[index]} {index} {isScrolling + ''}
-//     </div>
-//   )
-// }
-
 // 索引（A、B等）的高度
 const TITLE_HEIGHT = 36
 // 每个城市名称的高度
@@ -90,16 +65,29 @@ const formatCityIndex = letter => {
   }
 }
 
+// 有房源的城市
+const HOUSE_CITY = ['北京', '上海', '广州', '深圳']
+
 export default class CityList extends React.Component {
-  state = {
-    cityList: {},
-    cityIndex: [],
-    // 指定右侧字母索引列表高亮的索引号
-    activeIndex: 0
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      cityList: {},
+      cityIndex: [],
+      // 指定右侧字母索引列表高亮的索引号
+      activeIndex: 0
+    }
+
+    // 创建ref对象
+    this.cityListComponent = React.createRef()
   }
 
-  componentDidMount() {
-    this.getCityList()
+  async componentDidMount() {
+    await this.getCityList()
+
+    // 调用 measureAllRows，提前计算 List 中每一行的高度，实现 scrollToRow 的精确跳转
+    // this.cityListComponent.current.measureAllRows()
   }
 
   // 获取城市列表数据的方法
@@ -124,6 +112,16 @@ export default class CityList extends React.Component {
     })
   }
 
+  changeCity({ label, value }) {
+    if (HOUSE_CITY.indexOf(label) > -1) {
+      // 有
+      localStorage.setItem('hkzf_city', JSON.stringify({ label, value }))
+      this.props.history.go(-1)
+    } else {
+      Toast.info('该城市暂无房源数据', 1, null, false)
+    }
+  }
+
   // List组件渲染每一行的方法：
   rowRenderer = ({
     key, // Unique key within array of rows
@@ -143,7 +141,11 @@ export default class CityList extends React.Component {
       <div key={key} style={style} className="city">
         <div className="title">{formatCityIndex(letter)}</div>
         {cityList[letter].map(item => (
-          <div className="name" key={item.value}>
+          <div
+            className="name"
+            key={item.value}
+            onClick={() => this.changeCity(item)}
+          >
             {item.label}
           </div>
         ))}
@@ -164,7 +166,14 @@ export default class CityList extends React.Component {
     // 获取到 cityIndex，并遍历其，实现渲染
     const { cityIndex, activeIndex } = this.state
     return cityIndex.map((item, index) => (
-      <li className="city-index-item" key={item}>
+      <li
+        className="city-index-item"
+        key={item}
+        onClick={() => {
+          // console.log('当前索引号：', index)
+          this.cityListComponent.current.scrollToRow(index)
+        }}
+      >
         <span className={activeIndex === index ? 'index-active' : ''}>
           {item === 'hot' ? '热' : item.toUpperCase()}
         </span>
@@ -172,40 +181,39 @@ export default class CityList extends React.Component {
     ))
   }
 
+  // 用于获取List组件中渲染行的信息
+  onRowsRendered = ({ startIndex }) => {
+    // console.log('startIndex：', startIndex)
+    if (this.state.activeIndex !== startIndex) {
+      this.setState({
+        activeIndex: startIndex
+      })
+    }
+  }
+
   render() {
     return (
       <div className="citylist">
         {/* 顶部导航栏 */}
-        <NavBar
-          className="navbar"
-          mode="light"
-          icon={<i className="iconfont icon-back" />}
-          onLeftClick={() => this.props.history.go(-1)}
-        >
-          城市选择
-        </NavBar>
+        <NavHeader>城市选择</NavHeader>
 
         {/* 城市列表 */}
         <AutoSizer>
           {({ width, height }) => (
             <List
+              ref={this.cityListComponent}
               width={width}
               height={height}
               rowCount={this.state.cityIndex.length}
               rowHeight={this.getRowHeight}
               rowRenderer={this.rowRenderer}
+              onRowsRendered={this.onRowsRendered}
+              scrollToAlignment="start"
             />
           )}
         </AutoSizer>
 
         {/* 右侧索引列表 */}
-        {/* 
-          1 封装 renderCityIndex 方法，用来渲染城市索引列表。
-          2 在方法中，获取到索引数组 cityIndex ，遍历 cityIndex ，渲染索引列表。
-          3 将索引 hot 替换为 热。
-          4 在 state 中添加状态 activeIndex ，指定当前高亮的索引。
-          5 在遍历 cityIndex 时，添加当前字母索引是否高亮的判断条件。
-        */}
         <ul className="city-index">{this.renderCityIndex()}</ul>
       </div>
     )
