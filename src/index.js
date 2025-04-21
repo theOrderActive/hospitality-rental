d22fbac0851b5ba832e177cef1797ef12e0d5c79
theorder <2486711860@qq.com>
2025-04-21 23:03:23 +0800

    build

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 
 // 导入antd-mobile的样式：
-import 'antd-mobile/dist/antd-mobile.css'
+// import 'antd-mobile/dist/antd-mobile.css'
 
 // 导入字体图标库的样式文件
 import './assets/fonts/iconfont.css'
